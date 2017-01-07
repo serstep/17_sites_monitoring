78d6a959b6fea0a2cf157b5a9766c7047f8703cd
serstep <myserstep@mail.ru>
2017-01-07 19:50:24 +0200

    Update check_sites_health.py

--- check_sites_health.py
+++ check_sites_health.py
@@ -14,12 +14,7 @@ def get_domain_name_from_url(url):
 
 def is_server_respond_with_200(url):
     status_ok = 200
-    status_redirect = 302
-
-    response = requests.head(url)
-    
-    if response.status_code == status_redirect:
-        response = requests.head(response.headers["Location"])
+    response = requests.head(url, allow_redirects=True)
 
     return status_ok == response.status_code
 
@@ -29,11 +24,11 @@ def get_domain_expiration_date(domain_name):
     return(whois_response.expiration_date)
     
 
-def is_less_than_month_left(date):
-    days_in_month = 31
+def is_close_to_expire(date):
+    days_in_exp_offset = 31
     today = datetime.today()
-    month_duration = timedelta(days=days_in_month)
-    return month_duration > date - today
+    expiration_offset = timedelta(days=days_in_exp_offset)
+    return expiration_offset > date - today
 
 
 if __name__ == '__main__':
@@ -47,6 +42,6 @@ if __name__ == '__main__':
         domain_name = get_domain_name_from_url(url)
         domain_expiration_date = get_domain_expiration_date(domain_name)
 
-        close_to_expire = is_less_than_month_left(domain_expiration_date) if domain_expiration_date is not None else True
+        close_to_expire = is_close_to_expire(domain_expiration_date) if domain_expiration_date is not None else True
 
         print("URL: {}\nclose to expire: {}\nresponse 200: {}\n".format(url, close_to_expire, status_ok))
